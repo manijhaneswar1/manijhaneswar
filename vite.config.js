c9f0942bb276264cfc194af9b763aede5a074153
Mani Jhaneswar <manijhaneswar@gmail.com>
2026-02-11 17:18:28 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/manijhaneswar/', // matches your GitHub repo name exactly
-  plugins: [react(), tailwindcss()],
-})
